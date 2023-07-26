9716fcbfd1a780d1cca046d8d2cbd23b7c74f5f6
eyaefe-jennifer <eyaefejennifer@gmail.com>
2023-07-26 09:22:23 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ["./src/**/**/*.{html,jsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-gray": "#373737",
+      },
+    },
+  },
+  plugins: [
+    // ...
+    require("@tailwindcss/line-clamp"),
+  ],
+};
